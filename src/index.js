a607822e417fb1d9a7fed2911ecfd1435aeac233
Axel <maxelolsson@gmail.com>
2023-11-19 22:02:00 -0500

    Add future dependencies and prep for creating outline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash"
+import './style.css'
+
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'Briana'], ' ');
-  
+    // element.innerHTML = _.join(['Hello', 'Briana'], ' ');
+    element.textContent= "Hello Briana"
     return element;
   }
   
