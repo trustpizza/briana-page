a487ccbfcbec7121b4c2f0329597110e1d3a675b
Axel <maxelolsson@gmail.com>
2023-11-21 18:33:21 -0500

    Pausing at Using webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: {
     index: './src/index.js',
     homepage: "./src/homepage.js"
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   mode: "development",
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
